d4e2e5ce0f2e530caed64d85a020219c2d47e37a
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2021-09-10 16:43:59 +0800

    alvistack/4.0.0
        git clean -xdf
        tar zcvf ../python-typing-extensions_4.0.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-typing-extensions.spec ../python-typing-extensions_4.0.0-1.spec
        mv ../python*-typing-extensions*4.0.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/python-typing-4.0.0/
        rm -rf ../python*-typing-extensions*4.0.0*.* ../typing-extensions*4.0.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='typing_extensions',
+    version='4.0.0',
+    package_dir={'': 'typing_extensions/src_py3'},
+    py_modules=['typing_extensions'],
+)
