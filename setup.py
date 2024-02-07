caf4dda3e83c8237fd841d2cc131fd9c29ec55f4
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2024-02-07 12:15:52 +0800

    alvistack/3.7.4.3
        git clean -xdf
        tar zcvf ../python-typing-extensions_3.7.4.3.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-typing-extensions.spec ../python-typing-extensions_3.7.4.3-1.spec
        cp ../python*-typing-extensions*3.7.4.3*.{gz,xz,spec,dsc} /osc/home\:alvistack/python-typing-3.7.4.3/
        rm -rf ../python*-typing-extensions*3.7.4.3*.* ../typing-extensions*3.7.4.3*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- setup.py
+++ setup.py
@@ -12,60 +12,59 @@ if sys.version_info < (2, 7, 0) or (3, 0, 0) <= sys.version_info < (3, 4, 0):
 version = '3.7.4.3'
 description = 'Type Hints for Python'
 long_description = '''\
-Typing -- Type Hints for Python
+Typing Extensions -- Backported and Experimental Type Hints for Python
 
-This is a backport of the standard library typing module to Python
-versions older than 3.5.  (See note below for newer versions.)
+The ``typing`` module was added to the standard library in Python 3.5, but
+many new features have been added to the module since then.
+This means users of Python 3.5 - 3.6 who are unable to upgrade will not be
+able to take advantage of new types added to the ``typing`` module, such as
+``typing.Protocol`` or ``typing.TypedDict``.
 
-Typing defines a standard notation for Python function and variable
-type annotations. The notation can be used for documenting code in a
-concise, standard format, and it has been designed to also be used by
-static and runtime type checkers, static analyzers, IDEs and other
-tools.
+The ``typing_extensions`` module contains backports of these changes.
+Experimental types that will eventually be added to the ``typing``
+module are also included in ``typing_extensions``, such as
+``typing.ParamSpec`` and ``typing.TypeGuard``.
 
-NOTE: in Python 3.5 and later, the typing module lives in the stdlib,
-and installing this package has NO EFFECT, because stdlib takes higher
-precedence than the installation directory. To get a newer version of
-the typing module in Python 3.5 or later, you have to upgrade to a
-newer Python (bugfix) version.  For example, typing in Python 3.6.0 is
-missing the definition of 'Type' -- upgrading to 3.6.2 will fix this.
-
-Also note that most improvements to the typing module in Python 3.7
-will not be included in this package, since Python 3.7 has some
-built-in support that is not present in older versions (See PEP 560.)
-
-For package maintainers, it is preferred to use
-``typing;python_version<"3.5"`` if your package requires it to support
-earlier Python versions. This will avoid shadowing the stdlib typing
-module when your package is installed via ``pip install -t .`` on
-Python 3.5 or later.
+Users of Python versions before 3.5 should install and use
+the ``typing`` module from PyPI instead of using this one, unless specifically
+writing code that must be compatible with multiple Python versions or requires
+experimental types.
 '''
 
-package_dir = {2: 'python2', 3: 'src'}[sys.version_info.major]
-
 classifiers = [
-    'Development Status :: 5 - Production/Stable',
+    'Development Status :: 3 - Alpha',
     'Environment :: Console',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: Python Software Foundation License',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Topic :: Software Development',
 ]
 
-setup(name='typing',
+if sys.version_info.major == 2:
+    package_dir = 'typing_extensions/src_py2'
+elif sys.version_info.major == 3:
+    package_dir = 'typing_extensions/src_py3'
+else:
+    raise AssertionError()
+
+setup(name='typing_extensions',
       version=version,
       description=description,
       long_description=long_description,
-      author='Guido van Rossum, Jukka Lehtosalo, Łukasz Langa, Ivan Levkivskyi',
-      author_email='jukka.lehtosalo@iki.fi',
-      url='https://docs.python.org/3/library/typing.html',
-      project_urls={'Source': 'https://github.com/python/typing'},
+      author='Guido van Rossum, Jukka Lehtosalo, Łukasz Langa, Michael Lee',
+      author_email='levkivskyi@gmail.com',
+      url='https://github.com/python/typing/blob/master/typing_extensions/README.rst',
       license='PSF',
       keywords='typing function annotations type hints hinting checking '
                'checker typehints typehinting typechecking backport',
       package_dir={'': package_dir},
-      py_modules=['typing'],
-      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+      py_modules=['typing_extensions'],
       classifiers=classifiers)
